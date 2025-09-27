c76c0f7453fef294afdc49bb19abdb22b447710c
hammock1 <farasylvain1977@hotmail.com>
2025-09-27 08:34:52 -0400

    this ex. is to practice while() and for()

--- php/Conditionals/Demos/hello-hi-null-coalescing.php
+++ php/Conditionals/Demos/hello-hi-null-coalescing.php
@@ -1,13 +1,26 @@
 <?php
-  $greeting = $_GET['greeting'] ?? 'Hello';
+
+$greeting = $_GET['greeting'];
+
+print_r($greeting );
+
+if(isset($greeting)){
+
+ echo $greeting;
+
+}else{
+echo "thanks for converting the null coalescing to isset() <br> but your $greeting variable has not been set";
+
+}
+  //$greeting = $_GET['greeting'] ?? 'Hello';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="../../static/styles/normalize.css">
-<link rel="stylesheet" href="../../static/styles/styles.css">
+<link rel="stylesheet" href="../../styles/normalize.css">
+<link rel="stylesheet" href="../../styles/styles.css">
 <title><?= $greeting ?>, World!</title>
 </head>
 <body>
--- php/OperatorsAndLoops/Exercises/loops.php
+++ php/OperatorsAndLoops/Exercises/loops.php
@@ -6,25 +6,58 @@
 <link rel="stylesheet" href="../../static/styles/normalize.css">
 <link rel="stylesheet" href="../../static/styles/styles.css">
 <title>Loops</title>
+<style>
+  ul{
+    list-style-type:none;
+  }
+</style>
 </head>
 
 <body>
 <main>
-  <h2>while</h2>
+  <h2>Use while to print even numbers b/w 1-100</h2>
   <ul>
   <?php
     //Use a while loop to output all the even numbers
     //less than or equal to 100.
+    $i = 2;
+    $box = [];
+
+    while($i <= 100){
+
+      echo "<li>$i</li>";
+
+     // if($i % 2 === 0){
+
+
+      //  $box [] = $i;
+     // }
+
+
+      $i += 2;
+    }
+
+  //  echo "Even numbers: " . implode(", " , $box);
   ?>
   </ul>
 
   <h2>for</h2>
-  <ul>
+
+  <!--<ul>-->
   <?php
+  $store = [];
+  for ($i = 1; $i <= count(range(1,100)); $i+=2){
+
+    $store[] = $i;
+
+   
+  }
     //Use a for loop to output all the odd numbers
     //less than or equal to 100.
+
+  echo implode(", ", $store);
   ?>
-  </ul>
+  <!--</ul>-->
 </main>
 </body>
 </html>
