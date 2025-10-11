c33bdd8cf95cc8465f443d5186718c61dc38f2b0
hammock1 <farasylvain1977@hotmail.com>
2025-10-11 08:54:32 -0400

    practicing array and associative arrays

--- php/Arrays/Exercises/color-table-2.php
+++ php/Arrays/Exercises/color-table-2.php
@@ -6,17 +6,42 @@
 <link rel="stylesheet" href="../../static/styles/normalize.css">
 <link rel="stylesheet" href="../../static/styles/styles.css">
 <title>Color Table</title>
+<style>
+
+  h1{
+    text-align : center;
+  }
+
+  thead{
+    color : black;
+    text-align : left;
+  }
+  table{
+
+    border-collapse : collapse;
+    color:white;
+    
+  }
+</style>
 </head>
 <body>
 <main>
   <h1>Color Table</h1>
   <?php
     // Create an associative array that holds color hex codes
+    $colors =[
+      "green" => "#008800",
+      "black" => "#521d1dff",
+      "purple"=> "#4b0082",
+      "grey" => "#dddddd",
+      "yellow" => "#ddb113e0",
+      "blue" => "#000080",
+    ];
     // indexed by their color names, which can be found at
     // https://developer.mozilla.org/en-US/docs/Web/CSS/named-color
   ?>
 
-  <table>
+  <table width="500px" cellpadding="5" border="1" align="center">
     <thead>
       <tr>
         <th>Color Name</th>
@@ -25,7 +50,15 @@
     </thead>
     <tbody>
       <?php
+
         //Loop through the array outputting a table row with
+        foreach($colors as $color => $col){
+          echo "<tr style='background-color: $col'>";
+              echo "<td>$color</td>";
+              echo "<td>$col</td>";
+          echo "</tr>";
+
+        }
         //two columns for each element in the array
       ?>
     </tbody>
--- php/Arrays/Exercises/color-table.php
+++ php/Arrays/Exercises/color-table.php
@@ -6,10 +6,43 @@
 <link rel="stylesheet" href="../../static/styles/normalize.css">
 <link rel="stylesheet" href="../../static/styles/styles.css">
 <title>Color Table</title>
+<style>
+  .container{
+
+    width : 80%;
+ margin: 0 auto;
+
+  
+
+
+
+  }
+
+  main {
+
+    display : flex;
+    justify-content : center;
+    
+  }
+
+  .container h1{
+       text-align:left;
+
+  }
+
+  table{
+    margin-left : 0;
+    width : 70%;
+  }
+
+  
+</style>
 </head>
 <body>
 <main>
-  <h1>Color Table</h1>
+  <div class="container">
+
+      <h1>Color Table</h1>
   <?php
     // Create an array that holds your favorite colors.
 
@@ -25,7 +58,7 @@
     // A list of color names can be found at
     // https://developer.mozilla.org/en-US/docs/Web/CSS/named-color
   ?>
-  <table width="500" align="center" border="1" cellpadding="10" cellspacing="5">
+  <table width="500"  border="1" cellpadding="10" cellspacing="5">
     <thead>
       <tr>
         <th>Color</th>
@@ -36,7 +69,7 @@
       
       foreach($colors as  $color){
 
-        echo "<tr style='background : $color'><td align='center'><strong>$color</strong></td><tr>";
+        echo "<tr style='background : $color'><td ><strong>$color</strong></td><tr>";
 
 
 
@@ -46,6 +79,8 @@
       ?>
     </tbody>
   </table>
+
+  </div>
 </main>
 </body>
 </html>
